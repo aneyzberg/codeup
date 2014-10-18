f1b48a91a597f2edaa766fcb03548cbb0afc9970
Anna Neyzberg <aneyzberg@gmail.com>
2014-10-18 02:46:48 +0000

    first commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Codeup::Application.config.secret_key_base = '61974d7a3a62d72f03f29b151c4d64af6bc0c7445758b3cfb5d2b1cdd56eb9a5e1bdc5f631d633e06ee393561b93f1e96b137df76d7beb84214127171c244402'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Codeup::Application.config.session_store :cookie_store, key: '_codeup_session'
