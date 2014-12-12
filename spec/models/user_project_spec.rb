318c6ac1fdea247e6b7c2c85156a5c2f7cf9f350
Anna Neyzberg <aneyzberg@gmail.com>
2014-12-12 07:22:08 +0000

    created test for user_project validation

---
+++ spec/models/user_project_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+  describe "User Project" do
+    
+    it "is valid with user and project" do
+
+      user_project = create(:user_project)
+
+      expect(user_project).to be_valid
+    end
+  end
